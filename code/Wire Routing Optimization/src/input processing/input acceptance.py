7586f6ffa1819c57109997580725b94c00629baa
Vince Lebo <51753087+vlebovitz@users.noreply.github.com>
2022-04-03 15:19:57 -0500

    vincent added input processing

--- code/Wire Routing Optimization/src/input processing/input acceptance.py
+++ code/Wire Routing Optimization/src/input processing/input acceptance.py
@@ -1,12 +1,41 @@
-import csv
 import pandas as pd
-import obj.wire # import the wire class
 
-# read the csv in as a dataframe
-df = pd.read_csv('Wire Data Input.csv')
-print(df)
+#Implementation of Class Structuring
+class WireSystem:
+    def __init__(self,dataframe):
+        self.dataframe = pd.read_csv(dataframe)
+        
+    #Read in Data
+    def readData(self):
+        print(self.dataframe)
+        return self.dataframe
+    #Massage Data
+    def massageData(self):
+        df = self.dataframe
+        #Remove Unecessary Columns
+        df = df.drop(df.columns[[4,5]], axis=1)
+        #Organize Both X and Y Coordinates
+        #Start
+        startCoords = df['Start Point'].str.strip('()').str.split(',',expand=True)
+        df.insert(loc = 3, column = 'startX',value=startCoords[0])
+        df.insert(loc = 4, column = 'startY',value=startCoords[1])
+        df.insert(loc = 5, column = 'startZ',value=startCoords[2])
+        #End
+        df[['endX','endY','endZ']] = df['End Point'].str.strip('()').str.split(',',expand=True).rename(columns={0:'endX', 1:'endY',2:'endZ'})
+        self.dataframe = df
+        print(self.dataframe)
+        return self.dataframe
+    #Return wire information specified wire number
+    def wireInfo(self,wireNumber):
+        return self.dataframe.loc[self.dataframe['Wire Number']== wireNumber]
+    
+        
+        
+#Implement Function Calls for Instance A
+a = WireSystem('Wire Data Input.csv')
 
-# Access a column
-f = df["Wire Number"]
-
-
+#Read & Massage Data for Futher Analysis
+a.readData()
+a.massageData()
+#Print out the wire information
+a.wireInfo(2)

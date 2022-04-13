49039001f6794094098120d3f086ee2ace0ec06b
Aanchalaanchal <79412247+Aanchalaanchal@users.noreply.github.com>
2022-04-12 19:37:12 -0500

    Update TestPerDist.py

--- code/project/tests/TestPerDist.py
+++ code/project/tests/TestPerDist.py
@@ -1,13 +1,22 @@
 #RAISES ERRORS FOR NON-FLOAT INPUTS AS COORDINATES
+#BASIC CODE FOR PERPENDICULAR DISTANCES - FROM A SET OF POINTS
 
-import unittest
-from perpendicular_distances import per_dist
+import math
 
-class TestPerDist(unittest.TestCase):
-    def test_per_dist(self):
-        self.assertRaises(TypeError, per_dist, 'a')
-        self.assertRaises(TypeError, per_dist, True)
-        self.assertRaises(TypeError, per_dist, ['a','a'])
+def per_dist(A, B): #takes sets of points A and B
+    C = []
+    if len(A)==len(B):
+        for i in range(len(A)):
+            distance_squared = ((A[i][0]-B[i][0])**2)+((A[i][1]-B[i][1])**2)+((A[i][2]-B[i][2])**2)
+            distance = math.sqrt(distance_squared)
+            C.append(distance)
+        return C #returns a list of distances, indexed correspondingly
+    else:
+        print('error - missing values')
 
-if __name__--'__main__':
-    unittest.main()
+#DRIVER CODE
+
+#A = [[2,4,6],[8,10,2],[4,6,8]] #set 1
+#B = [[1,3,5],[7,9,11],[1,3,5]] #set 2
+#Dist = per_dist(A,B)
+#print(Dist)

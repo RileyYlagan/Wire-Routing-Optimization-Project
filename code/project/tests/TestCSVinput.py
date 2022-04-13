077a0b617947b8ecfaf78e77f33113d4741221b5
Aanchalaanchal <79412247+Aanchalaanchal@users.noreply.github.com>
2022-04-12 23:00:38 -0500

    Create TestCSVinput.py

---
+++ code/project/tests/TestCSVinput.py
@@ -0,0 +1,22 @@
+#BASIC CODE FOR CHECKING IF A .csv FILE IS ACTUALLY A .csv FILE
+
+import unittest
+
+from ______ import _____
+
+class TestDomainDiscret(unittest.TestCase):
+    def test_per_dist(self):
+        self.assertRaises(TypeError, _____, 'a')
+        self.assertRaises(TypeError, _____, True)
+        self.assertRaises(TypeError, _____, 1)
+        self.assertRaises(TypeError, _____, aanaa.csv)
+        self.assertRaises(TypeError, _____, aanaa.docx)
+        self.assertRaises(TypeError, _____, aanaa.xlsx)
+        self.assertRaises(TypeError, _____, aanaa.txt)
+        
+if __name__--'__main__':
+    unittest.main()
+    
+#Think about
+# [OUTPUT FILE NAME].lower().endswith('.csv')
+# if statement

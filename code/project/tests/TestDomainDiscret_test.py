a78611c10ff32565225369f10eab30e3548f2417
Aanchalaanchal <79412247+Aanchalaanchal@users.noreply.github.com>
2022-04-12 22:59:31 -0500

    Update and rename TestDomainDiscret.py to TestDomainDiscret_test.py

--- code/project/tests/TestDomainDiscret_test.py
+++ code/project/tests/TestDomainDiscret_test.py
@@ -9,7 +9,14 @@ class TestDomainDiscret(unittest.TestCase):
         self.assertRaises(TypeError, _____, 'a')
         self.assertRaises(TypeError, _____, True)
         self.assertRaises(TypeError, _____, 1)
-        self.assertRaises(TypeError, _____, non-stl file)
-
+        self.assertRaises(TypeError, _____, aanaa.csv)
+        self.assertRaises(TypeError, _____, aanaa.docx)
+        self.assertRaises(TypeError, _____, aanaa.xlsx)
+        self.assertRaises(TypeError, _____, aanaa.txt)
+        
 if __name__--'__main__':
     unittest.main()
+    
+#Think about
+# [OUTPUT FILE NAME].lower().endswith('.STL')
+# if statement
